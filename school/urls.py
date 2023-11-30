9765e23dd03be03b3c8687de25fa52a3b5f0a218
omengo <omengomanuel@gmail.com>
2023-11-30 08:08:30 +0300

    updated

--- school/urls.py
+++ school/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('about', views.about),
     path('contact', views.contact),
     path('loginpage', views.loginpage, name="loginpage"),
